e527d0a38a8a322ecb751f6749d8f5655355890c
Evan Davey <evan.j.davey@gmail.com>
2011-11-10 10:18:19 +0000

    adds measurement conversion code

--- recipemonkeyapp/utils/unitconversion.py
+++ recipemonkeyapp/utils/unitconversion.py
@@ -1,8 +1,25 @@
 
-
-def unitconversion(q1,m1,q2,m2):
-	"""  Converts quantity1 q1 in measure1 m1 to quantity2 q2 in measure """
+def convertToGrams(q,m):
 	
-	return 
+	"""
+		1 tablespoon = 3 teaspoons
+		1 cup = 16 tablespoons
+	"""
+	
+	weight_tablespoon=15
+	
+	m=m.lower().strip()
+	
+	if m=='teaspoon' or m=='teaspoons' or m=='tsp':
+		return (q*weight_tablespoon)/3
+
+	
+	if m=='tablespoon' or m=='tablespoons' or m=='tbsp':
+		return q*weight_tablespoon
+
+	if m=='cups' or m=='cup':
+		return q*weight_tablespoon*16
+
+	return q
 	
 	
