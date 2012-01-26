0db280b06238b6e090576a6465ee7dabd860d7da
Evan Davey <evan.j.davey@gmail.com>
2012-01-26 11:21:02 +0000

    adding storage index

---
+++ recipemonkeyapp/views/storage.py
@@ -0,0 +1,18 @@
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render_to_response,redirect
+from django.template import RequestContext
+from recipemonkeyapp.models import Storage
+
+
+
+def index(request):
+    	
+	item_list=Storage.objects.all()
+	
+	
+		
+	ct={'item_list':item_list,
+	
+	}
+	
+	return render_to_response('storage/index.html',ct,context_instance=RequestContext(request))
