582f3690b425b80e8e2b6a8a20b37cc3d6cc078d
Evan Davey <evan.j.davey@gmail.com>
2012-01-26 17:17:13 +0000

    moving groceries list to django-tables2

--- recipemonkeyapp/views/groceryitem.py
+++ recipemonkeyapp/views/groceryitem.py
@@ -27,7 +27,7 @@ def labels(request):
    
 
 class GroceryItemTable(tables.Table):
-    seasonStart=table.Columns(data='seasonStart').strftime('%M')
+    seasonStart=Table.Columns(data='seasonStart').strftime('%M')
     
     class Meta:
         model = GroceryItem
