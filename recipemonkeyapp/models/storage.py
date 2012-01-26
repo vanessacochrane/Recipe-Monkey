d8891ad09f316f964915823c3b925de2da871746
Evan Davey <evan.j.davey@gmail.com>
2012-01-26 11:26:48 +0000

    adding storage index

--- recipemonkeyapp/models/storage.py
+++ recipemonkeyapp/models/storage.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.contenttypes import generic
 from recipemonkeyapp.models.storageitem import StorageItem
+from django.contrib.contenttypes.models import ContentType
 
 class Storage(models.Model):
 	
