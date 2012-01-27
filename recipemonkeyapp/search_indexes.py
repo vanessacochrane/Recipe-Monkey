b5bbe0927fcae90c822326d455fda02a2e59004b
Evan Davey <evan.j.davey@gmail.com>
2012-01-27 08:24:35 +0000

    adding grocery search

--- recipemonkeyapp/search_indexes.py
+++ recipemonkeyapp/search_indexes.py
@@ -1,11 +1,14 @@
 import datetime
 from haystack.indexes import *
 from haystack import site
-from recipemonkeyapp.models import Recipe
+from recipemonkeyapp.models import Recipe,GroceryItem
 
 
 class RecipeIndex(RealTimeSearchIndex):
     text = CharField(document=True, use_template=True)
 
+class GroceryItemIndex(RealTimeSearchIndex):
+    text = CharField(document=True, use_template=True)
 
 site.register(Recipe, RecipeIndex)
+site.register(GroceryItem, GroceryItemIndex)
