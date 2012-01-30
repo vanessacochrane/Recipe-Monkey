5177bcaee065590667a487abebfa0e08af8abf52
Evan Davey <evan.j.davey@gmail.com>
2012-01-30 11:07:22 +0000

    added gh-pages for hosting docs

---
+++ docs/searchindex.js
@@ -0,0 +1,1 @@
+Search.setIndex({objects:{},terms:{recip:0,index:0,search:0,welcom:0,modul:0,indic:0,content:0,monkei:0,tabl:0,document:0,page:0},objtypes:{},titles:["Welcome to Recipe Monkey&#8217;s documentation!"],objnames:{},filenames:["index"]})
