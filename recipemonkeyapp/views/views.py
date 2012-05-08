6294844ed703b9d220f3a8a2e5523b70c8f3539d
Evan Davey <evan.j.davey@gmail.com>
2012-05-08 10:58:20 +0100

    adding in barcode pages

--- recipemonkeyapp/views/views.py
+++ recipemonkeyapp/views/views.py
@@ -36,10 +36,28 @@ def barcodes(request):
     response['Content-Disposition'] = 'attachment; filename=barcodes.pdf'
 
     barcodes_list=[]
-
+    tmp_folder = mkdtemp()
+    os.chdir(tmp_folder)        
+    texfile, texfilename = mkstemp(dir=tmp_folder)
     i=1
     for i in range(65):
-        barcodes_list.append(26)
+        url="https://%s/recipemonkeyapp/scan/%s/" % ('mothership.getoutsideandlive.com',i)
+
+        img=barcode('qrcode',url,data_mode='8bits',margin=0)
+        from PIL import Image
+        import numpy as np
+
+        pix = np.asarray(img)
+
+        pix = pix[:,:,0:3] # Drop the alpha channel
+        idx = np.where(pix-255)[0:2] # Drop the color when finding edges
+        box = map(min,idx)[::-1] + map(max,idx)[::-1]
+
+        region = img.crop(box)
+        f, fname = mkstemp(dir=tmp_folder)
+        
+        region.save(f, 'EPS')
+        barcodes_list.append(i)
 
     t = loader.get_template('recipemonkey/tex/barcodes.tex')
 
@@ -48,9 +66,7 @@ def barcodes(request):
     from tempfile import mkdtemp, mkstemp
     from django.template.loader import render_to_string
     # In a temporary folder, make a temporary file
-    tmp_folder = mkdtemp()
-    os.chdir(tmp_folder)        
-    texfile, texfilename = mkstemp(dir=tmp_folder)
+    
     # Pass the TeX template through Django templating engine and into the temp file
     os.write(texfile, render_to_string('recipemonkey/tex/barcodes.tex', {'barcodes': barcodes_list}))
     os.close(texfile)
