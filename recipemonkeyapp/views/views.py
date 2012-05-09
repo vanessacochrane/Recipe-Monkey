b7642e949cb71e8df64362785c4eb7138854c7a2
Evan Davey <evan.j.davey@gmail.com>
2012-05-09 19:34:25 +0100

    Improving scanning

--- recipemonkeyapp/views/views.py
+++ recipemonkeyapp/views/views.py
@@ -114,11 +114,11 @@ def ajax_object_request(request):
     # Expect an auto 'type' to be passed in via Ajax and POST
     #if request.is_ajax() and request.method == 'POST':
     
-    print request.GET
-    print request.GET.get('obj_type','')
+    #print request.GET
+    #print request.GET.get('obj_type','')
     
     if request.GET.get('obj_type','') == 'R':
-        print "r found"
+        #print "r found"
         objects = Recipe.objects.all() 
     else:
         objects = GroceryItem.objects.all() 
