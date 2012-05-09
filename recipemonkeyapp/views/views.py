c120eadfc432f8a7a944c25c4cd7231e45943e80
Evan Davey <evan.j.davey@gmail.com>
2012-05-09 09:55:14 +0100

    scanning

--- recipemonkeyapp/views/views.py
+++ recipemonkeyapp/views/views.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render_to_response
+from django.shortcuts import render_to_response,redirect
 from django.template import RequestContext
 from recipemonkeyapp.models import Recipe,GroceryItem
 from django.http import HttpResponse,HttpResponseRedirect
