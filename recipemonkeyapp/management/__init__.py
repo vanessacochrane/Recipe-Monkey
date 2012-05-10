72bcbdd3552381067fff5c8bd44436617bc59955
Evan Davey <evan.j.davey@gmail.com>
2012-05-10 16:43:41 +0100

    Adding expiry notifications

--- recipemonkeyapp/management/__init__.py
+++ recipemonkeyapp/management/__init__.py
@@ -1,5 +1,7 @@
 from django.conf import settings
 from django.utils.translation import ugettext_noop as _
+from django.db.models import signals
+
 
 if "notification" in settings.INSTALLED_APPS:
     from notification import models as notification
